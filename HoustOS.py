05719a54723bbf95edf4dcdcb56c34624c25b09d
Jeremy Morgan <galedragon1223@gmail.com>
2013-04-14 22:27:47 -0400

    Basic Python corrections

--- HoustOS.py
+++ HoustOS.py
@@ -1,40 +1,37 @@
 class HouseController:
-#contains threads for RoomListener objects, I cannot currently into threading
-#maybe each thread should run on a single pi unit, and then check for commands on the server?
+	# contains threads for RoomListener objects, I cannot currently into threading
+	# maybe each thread should run on a single pi unit, and then check for commands on the server?
 	pass
 
 class RoomListener:
-
-#basically, how does the object listen for voice commands and move between quiet and loud modes?
-	def self.listen():
+	#startup sequence
+	def __init__(self):
+		self.quiet = False
+		# temp gets handed off to an arduino for actual adjustments. 
+		# I currently think that we should keep all computation on the Pis
+		self.temperature = 70
+		self.shades = "closed"
+		self.door = "closed"
+		# door locks should be simple slide bolts that can be opened from the inside in case of emergency
+		self.doorLock = "open"
+		self.listen()
+		
+	#basically, how does the object listen for voice commands and move between quiet and loud modes?
+	def listen(self):
 		if self.quiet == True
 			listen_for_loud()
 		if self.quiet == False:
 			get_command():
 			
 
-#Example of simple call to room unit
-	def self.arduino_call():
-		call_correct_function(get_voice_command())
+	#Example of simple call to room unit
+	def arduino_call(self, command):
+		pass
   
-	def self.shades(state):
+	def shades(self, state):
 		if state == "open":
-			call_to_arduino()
+			self.call_to_arduino()
 		elif state == "close":
-			call_to_arduino()
-		
-#startup sequence
-	def __init__():
-		self.quiet = False 
+			self.call_to_arduino() 
 						
-		#temp gets handed off to an arduino for actual adjustments. I currently think that we should keep all computation on the Pis
-		self.Temperature = 70
 		
-		self.shades = "closed"
-		
-		self.door = "closed"
-		
-		#door locks should be simple slide bolts that can be opened from the inside in case of emergency
-		self.doorLock = "open"
-		
-		self.listen()
